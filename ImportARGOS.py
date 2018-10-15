46c399ae7a0bcea39651de9b5bece43caa492657
AmeliaMeier <ameliacmeier@gmail.com>
2018-10-15 12:15:59 -0400

    adding input variables

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -9,3 +9,10 @@
 ## Created: Fall 2018
 ## Author: amelia.meier@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
+
+# Set input variables (Hard-wired)
+inputFile = '../Data/ARGOSData/1997dg.txt'
+outputFC = "../scratch/ARGOStrack.shp"
